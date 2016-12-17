76107ccc4b1e66e165afee490205a5897705030c
dylanjsa90 <dylanjsanders1@gmail.com>
2016-12-17 15:44:13 -0800

    marking as completed not working

--- src/action_creators.js
+++ src/action_creators.js
@@ -1,25 +1,25 @@
-export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+// export const ADD_TODO = 'ADD_TODO';
+// export const TOGGLE_TODO = 'TOGGLE_TODO';
+// export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 let nextTodoId = 0
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+// export const VisibilityFilters = {
+//   SHOW_ALL: 'SHOW_ALL',
+//   SHOW_COMPLETED: 'SHOW_COMPLETED',
+//   SHOW_ACTIVE: 'SHOW_ACTIVE'
+// }
 
 export const addTodo = (text) => {
   return { 
-    type: ADD_TODO,
+    type: 'ADD_TODO',
     id: nextTodoId++,
     text 
   }
 }
 
 export const toggleTodo = (index) => {
-  return { type: TOGGLE_TODO, index }
+  return { type: 'TOGGLE_TODO', index }
 }
 
 export const setVisibilityFilter = (filter) => {
-  return { type: SET_VISIBILITY_FILTER, filter }
+  return { type: 'SET_VISIBILITY_FILTER', filter }
 }
--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,4 +1,5 @@
-const visibilityFilter = (state = SHOW_ALL, action) => {
+import { SET_VISIBILITY_FILTER } from '../action_creators'
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return  action.filter
